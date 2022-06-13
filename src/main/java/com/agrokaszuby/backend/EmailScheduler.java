1798b004f86ddf1f9bab864bb100b559cd2cb9b0
Katarzyna Nabożny <knnabozny@gmail.com>
2022-06-13 10:34:08 +0200

    F-7: Add scheduler

---
+++ src/main/java/com/agrokaszuby/backend/EmailScheduler.java
@@ -0,0 +1,36 @@
+package com.agrokaszuby.backend.scheduler;
+
+import com.agrokaszuby.backend.config.AdminConfig;
+import com.agrokaszuby.backend.domain.Mail;
+import com.agrokaszuby.backend.service.SimpleEmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@EnableScheduling
+@RequiredArgsConstructor
+public class EmailScheduler {
+
+    private static final String SUBJECT = "Agrokaszuby: daily Reservation report";
+    private static final String MESSAGE = "We are sending all date corresponding to new reservations";
+
+    private final SimpleEmailService simpleEmailService;
+    private final AdminConfig adminConfig;
+
+    @Scheduled(cron = "15 0 * * * *")
+    public void sendInformationEmail() {
+        simpleEmailService.send(
+                Mail.builder()
+                        .mailTo(adminConfig.getAdminMail())
+                        .mailFrom(adminConfig.getAdminMail())
+                        .subject(SUBJECT)
+                        .message(MESSAGE)
+                        .toCc(null)
+                        .build()
+        );
+    }
+
+}
